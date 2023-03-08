90e3d687f8e3e0a2ea4bedd2688064d6c3426317
Viacheslav ALEKSIEIEV <bagetta007@gmail.com>
2023-03-08 20:29:44 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,3 +14,5 @@ export const App = () => {
     </div>
   );
 };
+
+import './Profile/User'
---
+++ src/components/Profile/User.jsx
@@ -0,0 +1,10 @@
+import user from '../Profile'
+
+<Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/>
+console.log(Profile)
